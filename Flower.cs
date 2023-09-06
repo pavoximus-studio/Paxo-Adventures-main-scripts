4a1796588f27f90669da3cf07bdbeef18f13a27c
Pavoximus Studio <143937539+pavoximus-studio@users.noreply.github.com>
2023-09-06 17:23:32 +0200

    Update Flower.cs
    comments are added

--- Flower.cs
+++ Flower.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Flower : MonoBehaviour
 {
-    public GameObject ball;
-    public int shootforce;
-    private  Animator anim;
-    public Transform shootpoint;
-    public GameObject[] balls;
-    private float n = Mathf.PI / 6;
-    public Transform player;
-    private bool canshoot = true;
-    private Vector2 delta_vector;
+    public GameObject ball; //gameobject that flower shoots (in this case ball prefab that deals damage)
+    public int shootforce; //strength applied to force the bullets (balls) are shot
+    private  Animator anim; //animator
+    public Transform shootpoint; //the point from which the balls are fired
+    public GameObject[] balls; //array of multiple balls
+    private float n = Mathf.PI / 6; //30 degree angle
+    private bool canshoot = true; 
+    private Vector2 delta_vector; //aditional vector applied on top of main vector so that balls fire with some randomness
 
 
     void Start()
@@ -20,7 +19,7 @@ public class Flower : MonoBehaviour
         anim = GetComponent<Animator>();
         balls = new GameObject[5];
     }
-    IEnumerator Routine (float WaitTime)
+    IEnumerator Routine (float WaitTime) //what happens when player enters trigger circle of flower
     {
         canshoot = false;
         anim.SetBool("IsShot", true);
@@ -30,24 +29,24 @@ public class Flower : MonoBehaviour
         Firing();
     }
 
-    private void Firing()
+    private void Firing() //launching balls in multiple directions
     {
         for (int i = 0; i <= 4; i++)
         {
             float delta = Random.Range(n, 5 * n);
-            delta_vector = new Vector2(Mathf.Cos(delta), Mathf.Sin(delta));
-            balls[i] = Instantiate(ball, shootpoint.position, shootpoint.rotation);
-            Rigidbody2D rb = balls[i].GetComponent<Rigidbody2D>();
-            rb.AddForce(new Vector2(Mathf.Cos((i + 1) * n), Mathf.Sin((i + 1) * n)) * shootforce + delta_vector * 15);
+            delta_vector = new Vector2(Mathf.Cos(delta), Mathf.Sin(delta)); //smaller delta vector is made
+            balls[i] = Instantiate(ball, shootpoint.position, shootpoint.rotation); //"i" ball in array is instantiated at shootpoint
+            Rigidbody2D rb = balls[i].GetComponent<Rigidbody2D>(); //temporary rigidbody component for ball "i"
+            rb.AddForce(new Vector2(Mathf.Cos((i + 1) * n), Mathf.Sin((i + 1) * n)) * shootforce + delta_vector * 15); //main vectors are made with 30°, 60°, 90°, 120°, 150° angle. Intensity is adjusted with shootforce and delta vector is added on top of main vector for more natural look
         }
     }
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision) //Player is tagged Player and Routine starts if it enters trigger collider
     {
 
         if (collision.gameObject.tag == "Player")
         {
-            if (canshoot) 
+            if (canshoot)
             {
                 StartCoroutine(Routine(1));
             }
